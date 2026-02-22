ed8d12bfd65e6e44e1433d0c5c0c481f4dc6beda
GIRI ABHIJEET - MUMBAI <24481036@mahindra.com>
2026-02-22 19:35:52 +0530

    added code files

---
+++ models/User.ts
@@ -0,0 +1,27 @@
+import { Schema, model, models } from "mongoose";
+ 
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: [true, "Email already exists"],
+      required: [true, "Email is required"],
+    },
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+    },
+    userType: {
+        type: String,
+        enum: ["user", "admin", "service_provider"],
+        default: "user",
+    }
+  },
+  {
+    timestamps: true,
+  },
+);
+ 
+const User = models.User || model("User", UserSchema);
+ 
+export default User;
