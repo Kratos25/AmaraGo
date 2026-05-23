00abfe26a9b5fd134d30074ba62af850cc5ceb24
GIRI ABHIJEET - MUMBAI <24481036@mahindra.com>
2026-05-24 01:23:44 +0530

    added index.ts

---
+++ app/(client)/client/home/_types/index.ts
@@ -0,0 +1,14 @@
+// app/(client)/client/home/_types/index.ts
+
+export interface Service {
+  id: string;
+  name: string;
+  duration: string;
+  rating: number;
+  discountedPrice: string;
+  originalPrice: string;
+  discount: string;
+  rawDiscounted: number;
+  rawOriginal: number;
+  imageUrl?: string;
+}
